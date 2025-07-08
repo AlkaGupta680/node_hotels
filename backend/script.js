4196e6fda97dc21c1f7a9303bf06727ce1b4660f
Alka Gupta <140833937+AlkaGupta680@users.noreply.github.com>
2025-07-08 15:35:00 +0530

    Fix backend folder structure and file paths

--- backend/script.js
+++ backend/script.js
@@ -16,3 +16,36 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT||3000  // THIS IS IMPORT FROM .ENV
+
+app.use(passport.initialize()) ;
+
+const localAuthMiddleware =passport.authenticate('local',{session:false}) ;
+
+//GET METHOD of home
+app.get('/', function (req, res) {
+  res.send('welcome to my hotel')
+})
+
+//MiddleWare function 
+const logRequest = (req,res,next)=>{
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`[${new Date().toLocaleString()}] Request made to : ${req.originalUrl},Visitor IP: ${ip}`) ;
+  next() ;
+}
+
+//it is connect to all apis 
+app.use(logRequest)
+
+//IMPORT person router file 
+const personRoutes = require('./routes/personRoutes');
+//use routers
+app.use('/person',personRoutes) ;
+
+//IMPORT menuitem router file 
+const MenuItemRoutes = require('./routes/menuitemRoutes');
+//use routers
+app.use('/MenuItem' ,MenuItemRoutes) ;
+
+app.listen(PORT , ()=>{
+    console.log("server is listening on port 3000")
+})
